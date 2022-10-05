12e7f28f3af10bff20d227088e0b5dccbfcb5787
Keerthy Kusumam <keerthy.kusumam@astrazeneca.com>
2022-10-05 14:38:45 +0100

    add test cases for daily min and daily max functions

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import numpy.testing as npt
-
+import pytest
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -28,4 +28,44 @@ def test_daily_mean_integers():
 
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
+
+def test_daily_min_zeros():
+    """Test the min function works for an array of zeros"""
+    from inflammation.models import daily_min
+
+    test_input = np.array([[0,0],
+                           [0,0],
+                           [0,0],
+                          ])
+    test_result = np.array([0,0])
+
+    npt.assert_array_equal(daily_min(test_input),test_result)
+
+def test_daily_min_negative():
+    from inflammation.models import daily_min
+
+    test_input = np.array([[-1,5],
+                           [-2,4],
+                           [-6,6]])
+
+    test_result = np.array([-6,4])
+
+    npt.assert_array_equal(daily_min(test_input),test_result)
+
+def test_daily_max_negative():
+    from inflammation.models import daily_max
+
+    test_input = np.array([[-1,5],
+                           [-2,4],
+                           [-6,6]])
+
+    test_result = np.array([-1,6])
+
+    npt.assert_array_equal(daily_max(test_input),test_result)
+
+def test_daily_max_type_error():
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([["Hello","There"],["general","Min"]])
 
