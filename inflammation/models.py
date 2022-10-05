14c18c7fe58d3f5fc181f2385e5882ee5a2cbd71
Keerthy Kusumam <keerthy.kusumam@astrazeneca.com>
2022-10-05 13:59:46 +0100

    change models

--- inflammation/models.py
+++ inflammation/models.py
@@ -2,8 +2,8 @@
 
 The Model layer is responsible for the 'business logic' part of the software.
 
-Patients' data is held in an inflammation table (2D array) where each row contains 
-inflammation data for a single patient taken over a number of days 
+Patients' data is held in an inflammation table (2D array) where each row contains
+inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 """
 
@@ -11,7 +11,7 @@ import numpy as np
 
 
 def load_csv(filename):
-    """Load a Numpy array from a CSV
+    """Load a Numpy array from a CSV file
 
     :param filename: Filename of CSV to load
     """
