12c2783eb5eee49cf4c796362dafa2b25012c2cb
Dougan McKellar <dougan.mckellar@astrazeneca.com>
2022-10-27 14:56:33 +0100

    Removed commented lines of code.

--- tests/test_models.py
+++ tests/test_models.py
@@ -61,19 +61,7 @@ def test_daily_max(test,expected):
 
     npt.assert_array_equal(daily_max(np.array(test)),np.array(expected))
 
-'''@pytest.mark.parametrize(
-    "test, expected",
-    [
-        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]),
-        ([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [[1, 1, 1], [1, 1, 1], [1, 1, 1]]),
-        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]]),
-    ])
-def test_patient_normalise_simple(test, expected):
-    """Test normalisation works for arrays of one and positive integers.
-       Assumption that test accuracy of two decimal places is sufficient."""
-    from inflammation.models import patient_normalise
-    npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
-'''
+
 @pytest.mark.parametrize(
     "test, expected, expect_raises",
     [
@@ -115,32 +103,3 @@ def test_patient_normalise(test, expected, expect_raises):
             npt.assert_almost_equal(patient_normalise(test), np.array(expected), decimal=2)
     else:
         npt.assert_almost_equal(patient_normalise(test),np.array(expected), decimal=2)
-
-'''def test_daily_min_negative():
-    from inflammation.models import daily_min
-
-    test_input = np.array([[-1,5],
-                           [-2,4],
-                           [-6,6]])
-
-    test_result = np.array([-6,4])
-
-    npt.assert_array_equal(daily_min(test_input),test_result)
-
-def test_daily_max_negative():
-    from inflammation.models import daily_max
-
-    test_input = np.array([[-1,5],
-                           [-2,4],
-                           [-6,6]])
-
-    test_result = np.array([-1,6])
-
-    npt.assert_array_equal(daily_max(test_input),test_result)
-def test_daily_max_type_error():
-    from inflammation.models import daily_min
-
-    with pytest.raises(TypeError):
-        error_expected = daily_min([["Hello","There"],["general","Min"]])
-
-'''
